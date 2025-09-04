115defb6bb2b6b8caaa67822eac6ae57d9be09a9
Ivan <ivan2006lysenko@gmail.com>
2025-09-04 17:40:11 +0300

    did 12.1

---
+++ 12th Lesson/firstTask.js
@@ -0,0 +1,14 @@
+// На сторінці є дві кнопки. 
+// При натисканні на першу кнопку користувач повинен ввести в prompt посилання, 
+// при натисканні на другу – переадресовується на інший сайт (за раніше введеним посиланням).
+
+function firstTask() {
+    let myLink;
+    const myprompt = document.querySelector("#prompt").addEventListener("click", () => {
+        myLink = prompt("Введіть посилання")
+    })
+
+    const pereadres = document.querySelector("#nextS").addEventListener("click", () => {
+        window.location.href = myLink
+    })
+}
