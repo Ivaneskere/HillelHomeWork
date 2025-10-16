4c2932f0ddd2b3a4c3d606fc15baec83fe369755
Ivan <ivan2006lysenko@gmail.com>
2025-10-16 17:24:13 +0300

    remake 19th Lesson

--- 19th Lesson/main.js
+++ 19th Lesson/main.js
@@ -8,84 +8,57 @@
 // Завдання з *: При кліці на сутність - відображати деталі по ній (персонаж, планета, транспорт)
 
 
-
-let peopleUrl = 'https://swapi.dev/api/people/';
-let planetsUrl = 'https://swapi.dev/api/planets/';
-let vehiclesUrl = 'https://swapi.dev/api/vehicles/';
 const peopleContainer = document.querySelector(".infoBlockPeople")
 const planetsContainer = document.querySelector(".infoBlockPlanets")
 const vehiclesContainer = document.querySelector(".infoBlockVehicles")
+
 const peopleButt = document.querySelector('.peopleButton')
 const planetsButt = document.querySelector('.planetsButton')
 const vehiclesButt = document.querySelector('.vehiclesButton')
+
 const loadMorePeople = document.querySelector('.loadMorePeop')
 const loadMorePlan = document.querySelector('.loadMorePlan')
 const loadMoreVehicles = document.querySelector('.loadMoreVehicles')
 
-function load(type) {
-    if (type === "people") {
-        fetch(peopleUrl)
-            .then(res => res.json())
-            .then(result => {
-                showPeople(result.results);
-            }); 
-    } else if (type === "planets") {
-        fetch(planetsUrl)
-            .then(res => res.json())
-            .then(result => {
-                showPlanet(result.results);
-            });
-    } else if (type === "vehicles") {
-        fetch(vehiclesUrl)
-            .then(res => res.json())
-            .then(result => {
-                showVehicles(result.results);
-            });
+const entities = {
+    people: {
+        url: 'https://swapi.dev/api/people/',
+        container: peopleContainer,
+        button: peopleButt,
+        loadmore: loadMorePeople
+    },
+    planets: {
+        url: 'https://swapi.dev/api/planets/',
+        container: planetsContainer,
+        button: planetsButt,
+        loadmore: loadMorePlan
+    },
+    vehicles: {
+        url: 'https://swapi.dev/api/vehicles/',
+        container: vehiclesContainer,
+        button: vehiclesButt,
+        loadmore: loadMoreVehicles
     }
 }
 
+const load = function (type) {
+    fetch(entities[type].url)
+        .then(res => res.json())
+        .then(result => {
+            showAll(result.results, entities[type].container)
+            entities[type].url = result.next
+        })
+}
 
-function showPeople(people) {
-    people.forEach(person => {
+const showAll = function(data, container) {
+    data.forEach(item => {
         const element = document.createElement('li');
-        element.textContent = person.name;
-        peopleContainer.appendChild(element);
+        element.textContent = item.name;
+        container.appendChild(element);
     })
 }
-peopleButt.addEventListener('click', () => {
-    load('people');
-});
 
-loadMorePeople.addEventListener('click', () => {
-    load('people');
-})
-
-function showPlanet(planet) {
-    planet.forEach(planett => {
-        const element = document.createElement('li');
-        element.textContent = planett.name;
-        planetsContainer.appendChild(element);
-    })
-}
-planetsButt.addEventListener('click', () => {
-    load('planets');
-})
-
-loadMorePlan.addEventListener('click', () => {
-    load('planets');
-})
-
-function showVehicles(vehicles) {
-    vehicles.forEach(auto => {
-        const element = document.createElement('li')
-        element.textContent = auto.name;
-        vehiclesContainer.appendChild(element)
-    })
-}
-vehiclesButt.addEventListener('click', () => {
-    load('vehicles');
-})
-
-loadMoreVehicles.addEventListener('click', () => {
-    load('vehicles');
+Object.keys(entities).forEach(type => {
+    entities[type].button.addEventListener('click', () => load(type))
+    entities[type].loadmore.addEventListener('click', () => load(type))
 })
